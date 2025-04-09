c548f5f19212a4d75b69313cb657b20fe8e98d68
froku <froku@Section37.mshome.net>
2025-04-09 09:53:03 +0200

    First commit

---
+++ src/gittesting/GitTesting.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gittesting;
+
+/**
+ *
+ * @author froku
+ */
+public class GitTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        System.out.println("Hello Git");
+        System.out.println("Hello Section");
+    }
+    
+}
